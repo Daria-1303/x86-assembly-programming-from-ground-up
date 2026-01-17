68bda40aa094fbc179f6903c1b364273aba3cfff
Daria-1303 <daria.ciobanu@student.upt.ro>
2026-01-17 19:10:47 +0200

    section 10 done

---
+++ Section_10/IntegersExploration/IntegersExploration.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+
+extern  "C"     char    GlChar = 10;
+extern  "C"     short   GlShort = 20;
+extern  "C"     int     GlInt = 30;
+extern  "C"     long long   GlLongLong = 344566;
+
+extern  "C"     void    IntegerAddition(char a, short b, int c, long long d);
+
+int main()
+{
+    IntegerAddition(3, 5, -37, 11);
+
+    printf("Char value %d\n", GlChar);
+    printf("Short value %d\n", GlShort);
+    printf("Int value %d\n", GlInt);
+    printf("LongLong value %lld\n", GlLongLong);
+
+    return 0;
+}
+
+
